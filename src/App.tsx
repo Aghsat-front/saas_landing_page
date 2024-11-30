8c7eb3fc651283d2f795686f594d737eaab7ac8c
AghsatMarket Front <aghsatfront@gmail.com>
2024-11-30 05:21:33 +0000

    project completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import Download from './components/Download'
+import Faq from './components/Faq'
 import Features from './components/Features'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Pricing from './components/Pricing'
@@ -12,6 +15,9 @@ function App() {
         <Hero/>
         <Features/>
         <Pricing/>
+        <Faq/>
+        <Download/>
+        <Footer/>
       </main>
     </>
   )
